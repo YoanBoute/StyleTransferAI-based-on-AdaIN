a65b9fe2c5000a51c488fcbe1d9c3cb42cd2814b
bouteyoa <yoan.boute@etu.utc.fr>
2025-02-05 23:40:49 +0100

    First sketches of AdaIN layer and VGG Encoder part

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # StyleTransferAI-based-on-AdaIN
-Personal project on the implementation of the AdaIN paper in Tensorflow/Keras for neural style transfer
+Personal project on the implementation of the AdaIN paper in Keras/PyTorch for neural style transfer
---
+++ adain_layer.py
@@ -0,0 +1,46 @@
+import numpy as np
+import os
+
+os.environ["KERAS_BACKEND"] = "torch"
+from keras.layers import Layer
+from keras import ops
+
+
+class AdaINLayer(Layer) :
+    def __init__(self) :
+        super().__init__()
+    
+    # The AdaIN layer has no trainable weight, so it doesn't need to be built
+    def build(self, input_shape) :
+        pass
+
+    def call(self, inputs) :
+        content_input = inputs[0]
+        style_input = inputs[1]
+
+        return self.instance_std(style_input) * ((content_input - self.instance_mean(content_input)) / self.instance_std(content_input)) + self.instance_mean(style_input)
+
+
+    def instance_mean(self, batch) :
+        shape = batch.shape
+        match len(shape) :
+            case 4 :
+                pass
+            case 3 :
+                batch = batch.unsqueeze(0)
+            case _ :
+                raise ValueError("Incorrect shape for the provided batch. Expected a 3- or 4-dimensional Tensor.")
+        
+        return ops.mean(batch, [2,3], keepdims=True)
+
+    def instance_std(self, batch) :
+        shape = batch.shape
+        match len(shape) :
+            case 4 :
+                pass
+            case 3 :
+                batch = batch.unsqueeze(0)
+            case _ :
+                raise ValueError("Incorrect shape for the provided batch. Expected a 3- or 4-dimensional Tensor.")
+        
+        return ops.std(batch, [2,3], keepdims=True)
