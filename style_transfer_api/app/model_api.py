167688347140dca257fc599c0781b3d4ec904655
bouteyoa <yoan.boute@etu.utc.fr>
2025-04-13 23:09:37 +0200

    Handle request cancellation in API

--- style_transfer_api/app/model_api.py
+++ style_transfer_api/app/model_api.py
@@ -14,52 +14,138 @@ from fastapi import FastAPI, File, UploadFile, WebSocket
 from fastapi.responses import FileResponse
 import base64
 import json
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import Process, Queue
+import asyncio
+import psutil
+import time
+import numpy as np
 
 MLFLOW_TRACK_URI = Path("D:/Python/ML Flow/mlruns")
 TMP_DIR = Path("./__tmp")
 
 TMP_DIR.mkdir(exist_ok=True)
 
-model = AdaINModel.load_registered_model("StyleTransfer", "champion", MLFLOW_TRACK_URI)
+def switch_model_to_device(device, model) :
+    keras.src.backend.common.global_state.set_global_attribute("torch_device", device)
+    model = model.to(device)
+    model.device = device
+    return model
+
+def generate_img(content, style, result_queue, **params) :
+    model = AdaINModel.load_registered_model("StyleTransfer", "champion", MLFLOW_TRACK_URI)
+    model = switch_model_to_device("cpu", model)
+    stylized = model.generate(content, style, resize_size = 500, **params).numpy()
+    result_queue.put(stylized)
+
+class RequestManager :
+    """Class to manage the generation tasks, and cancel a request if a new request is emitted from the same client"""
+    def __init__(self):
+        self.tasks = {}
+        self.processes = {}
+        self.executor = ProcessPoolExecutor(max_workers=1)
+    
+    async def cancel_task(self, request_id) :
+        if request_id in self.tasks :
+            if request_id in self.processes :
+                process = self.processes[request_id]
+                try:
+                    process.terminate() 
+                    process.join()  # Wait for the process to clean up
+                except psutil.NoSuchProcess:
+                    pass
+                del self.processes[request_id]
+            self.tasks[request_id].cancel() 
+            try :
+                await self.tasks[request_id]
+            except asyncio.CancelledError :
+                pass            
+            del self.tasks[request_id]
+
+    async def start_task(self, request_id, content, style) :
+        await self.cancel_task(request_id)
+        res_queue = Queue()   
+        process = Process(target=generate_img, args=(content, style, res_queue)) 
+        process.start()
+        self.processes[request_id] = process
+        task = asyncio.create_task(self._monitor_task(process, res_queue, request_id))
+        self.tasks[request_id] = task
+        return task
+    
+    async def _monitor_task(self, process, result_queue, request_id) :
+        loop = asyncio.get_running_loop()
+        result = await loop.run_in_executor(None, result_queue.get)
+        process.join() # Make sure the process is cleaned up after it gave its result
+        del self.processes[request_id]
+        del self.tasks[request_id]
+        return result
+    
 
 app = FastAPI()
+manager = RequestManager()
 
 @app.get("/")
 def test() :
     return {"msg" : "Hello world"}
 
-@app.post("/generate")
-def generate(img) :
-    return {"Euuhhhh" : "Alors oui"}
-
-@app.post('/retest') 
-def retest(file : UploadFile) :
-    return FileResponse(Path('D:/StyleTransferAI/StyleTransferAI/test_images/lion.jpg'))
-
-@app.websocket('/ws')
+@app.websocket('/generate') # TODO : Use POST instead of WebSocket to transfer files (Maybe with AJAX)
 async def endpoint(ws : WebSocket) :
+    async def generation(request_id, content, style) :
+        gen_path = TMP_DIR / ('gen_' + str(np.random.randint(1e6)) + '.jpg')
+        gen_task = await manager.start_task(request_id, content, style)
+        try :
+            gen_img = await gen_task
+            keras.utils.save_img(gen_path, gen_img)
+            with open(gen_path, 'rb') as f :
+                gen_data = base64.b64encode(f.read()).decode('utf-8')
+            response = {
+                "request_id" : request_id,
+                "status" : "success",
+                "generated_image" : gen_data
+            }
+            content.unlink(missing_ok=True)
+            style.unlink(missing_ok=True)
+            gen_path.unlink(missing_ok=True)
+            await ws.send_text(json.dumps(response))
+        except asyncio.exceptions.CancelledError :
+            response = {
+                "request_id" : request_id,
+                "status" : "cancelled"
+            }
+            await ws.send_text(json.dumps(response))
+        except Exception as e :
+            response = {
+                "request_id" : request_id,
+                "status" : "error",
+                "message" : str(e)
+            }
+            await ws.send_text(json.dumps(response))
+        finally :    
+            content.unlink(missing_ok=True)
+            style.unlink(missing_ok=True)
+            gen_path.unlink(missing_ok=True)
+        
     await ws.accept()
     while True:
-        data = await ws.receive_text()
-        message = json.loads(data)
-        content = base64.b64decode(message["content"])
-        content_path = TMP_DIR / 'content.jpg'
-        with open(content_path, 'wb') as f :
-            f.write(content)
-        style = base64.b64decode(message["style"][0])
-        style_path = TMP_DIR / 'style.jpg'
-        with open(style_path, 'wb') as f :
-            f.write(style)
+        request_id = "unknown"
         try :
-            stylized_img = model.generate(content_path, style_path)
+            data = await ws.receive_text()
+            message = json.loads(data)
+            request_id = message["request_id"]
+            content = base64.b64decode(message["content"])
+            content_path = TMP_DIR / ('content_' + str(np.random.randint(1e6)) + '.jpg') # TODO : use UUID as basis for file names
+            # TODO : Retrieve file true extension to use it for saving the file
+            with open(content_path, 'wb') as f :
+                f.write(content)
+            style = base64.b64decode(message["style"][0])
+            style_path = TMP_DIR / ('style_' + str(np.random.randint(1e6)) + '.jpg')
+            with open(style_path, 'wb') as f :
+                f.write(style)
+            asyncio.create_task(generation(request_id, content=content_path, style=style_path))            
         except Exception as e :
-            print(e)
-        gen_path = TMP_DIR / "gen.jpg"
-        keras.utils.save_img(gen_path, stylized_img)
-        with open(gen_path, 'rb') as f :
-            gen_data = f.read()
-        await ws.send_bytes(gen_data)
-        content_path.unlink()
-        style_path.unlink()
-        gen_path.unlink()
-
+            response = {
+                "request_id" : request_id,
+                "status" : "error",
+                "message" : str(e)
+            }
+            await ws.send_text(json.dumps(response))
--- style_transfer_api/client/api_client.py
+++ style_transfer_api/client/api_client.py
@@ -8,35 +8,59 @@ import json
 
 img1 = Path('D:/StyleTransferAI/StyleTransferAI/test_images/sunset2.jpg')
 img2 = Path('D:/StyleTransferAI/StyleTransferAI/test_images/impressionisme.jpg')
+img3 = Path('D:/StyleTransferAI/StyleTransferAI/test_images/scream.jpg')
 
+# TODO : Add an authentification method (token / connection / ...)
 
-async def hello():
-    async with connect("ws://localhost:8000/ws", max_size=5*1024*1024) as websocket:
+async def generate() :
+    async with connect("ws://localhost:8000/generate", max_size=5*1024*1024) as websocket : # TODO : Change port number
         with open(img1, 'rb') as f :
             data1 = base64.b64encode(f.read()).decode("utf-8")
         with open(img2, 'rb') as f :
             data2 = base64.b64encode(f.read()).decode("utf-8")
-        message = {
+        with open(img3, 'rb') as f :
+            data3 = base64.b64encode(f.read()).decode("utf-8")
+        message1 = { # TODO : Add client ID
+            "request_id" : 'test1',
             "content" : data1,
             "style" : [
                 data2
             ]
         }
-        await websocket.send(json.dumps(message))
-        new_data = await websocket.recv()
-        with open(Path('C:/users/yoanb/Desktop/resp.jpg'), 'wb') as f :
-            f.write(new_data)
-        print("Data received !")
+        message2 = { 
+            "request_id" : 'test1',
+            "content" : data1,
+            "style" : [
+                data3
+            ]
+        }
+        print("Send first request")
+        await websocket.send(json.dumps(message1))
+        await asyncio.sleep(5)
+        print("Send second request")
+        await websocket.send(json.dumps(message2))
+        # response = await websocket.recv()
+        while True:
+            try:
+                response = await websocket.recv()
+                msg = json.loads(response)
+                print("Received:", msg["status"])
+                if msg.get("status") in ("success"):
+                    with open(Path('C:/users/yoanb/Desktop/resp.jpg'), 'wb') as f :
+                        f.write(base64.b64decode(msg["generated_image"]))
+            except websockets.ConnectionClosed:
+                print("WebSocket closed.")
+                break
 
 async def loop() :
     while True :
         i = input()
         if i == 'g' :
-            await hello()
+            await generate()
         elif i == 'exit' :
             quit()
         else :
             continue
 
 if __name__ == "__main__":
-    asyncio.run(loop())
+    asyncio.run(generate())
